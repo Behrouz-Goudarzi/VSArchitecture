4ca96a5233988dc785e41db398e005aa27a4f1fb
b.godarzi <b.godarzi@sadad.co.ir>
2024-10-30 13:59:36 +0330

    Add web api

---
+++ src/WebAPI/Program.cs
@@ -0,0 +1,22 @@
+using ProductManagement;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddProductManagement(builder.Configuration);
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseProductManagementEndpoints();
+
+app.Run();
+
